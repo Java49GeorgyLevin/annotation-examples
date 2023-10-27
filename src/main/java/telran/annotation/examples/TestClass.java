f2c31db8fa9ff7d42c1486a889df7311dbe321f0
Georgy Levin <g.phyzy@gmail.com>
2023-10-27 22:13:52 +0300

    HW 54: BeforeEach.class to Run

--- src/main/java/telran/annotation/examples/TestClass.java
+++ src/main/java/telran/annotation/examples/TestClass.java
@@ -2,7 +2,21 @@ package telran.annotation.examples;
 
 import telran.test.annotation.Test;
 
+import telran.test.annotation.BeforeEach;
+
 public class TestClass {
+
+	@BeforeEach
+	void printBeforeEach() {
+		System.out.println("I do print before each method");
+	}
+	@BeforeEach
+	void printBeforeEach2() {
+		System.out.println("I too do print");
+	}
+	void setUpFoo() {
+		System.out.println("I do SetUp Foo");
+	}	
 	private void f1(int b) { 
 		System.out.println("test f1 passed " + b);
 	}
