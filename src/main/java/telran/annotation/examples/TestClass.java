405bf291ab743a824c80b486a12f7b83df1f99b2
Georgy Levin <g.phyzy@gmail.com>
2023-10-30 22:01:41 +0200

    CW #55 example applying annotation @value

--- src/main/java/telran/annotation/examples/TestClass.java
+++ src/main/java/telran/annotation/examples/TestClass.java
@@ -1,35 +1,26 @@
 package telran.annotation.examples;
 
+import telran.test.annotation.BeforeEach;
 import telran.test.annotation.Test;
 
-import telran.test.annotation.BeforeEach;
-
 public class TestClass {
-
-	@BeforeEach
-	void printBeforeEach() {
-		System.out.println("I do print before each method");
-	}
-	@BeforeEach
-	void printBeforeEach2() {
-		System.out.println("I too do print");
-	}
-	void setUpFoo() {
-		System.out.println("I do SetUp Foo");
-	}	
 	private void f1(int b) { 
 		System.out.println("test f1 passed " + b);
 	}
 	private void f1() {
 		System.out.println("test f1 failed ");
 	}
-	@Test
+	@Test(nRuns = 2)
 	void test1() {
 		f1(10);
 	}
 	@Test
-	void test() {
+	void test10() {
 		f1();
+	}
+	@BeforeEach
+	void setUp() {
+		System.out.println("set up");
 	}
 	
 }
